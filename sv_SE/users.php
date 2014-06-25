e0cfd34be0a0865e4fa82bccb0e7891caa7d68bb
Kristoffer Malmström <mitod1@gmail.com>
2014-06-25 11:24:58 +0200

    updated users.php

--- sv_SE/users.php
+++ sv_SE/users.php
@@ -2,68 +2,67 @@
 
 return array(
 
-	'add_user' => 'L&auml;gg till ny anv&auml;ndare',
-	'recover_password' => '&Aring;terh&auml;mta l&ouml;senord',
 	'users' => 'Anv&auml;ndare',
+
 	'create_user' => 'Skapa ny anv&auml;ndare',
-	'reset_password' => '&Aring;terst&auml;ll l&ouml;senord',
-	'reset_password_for' => 'L&ouml;senordet &aring;terst&auml;llt f&ouml;r ',
-	'reset_password_explain' => 'Var god ange ett nytt l&ouml;senord (som du inte kommer gl&ouml;mma bort den h&auml;r g&aring;ngen.).',
-	'back_to' => 'Tillbaka till ',
-
-	// english spelling confusion
-	'log_in' => 'Logga in', // verb
-	'login' => 'Inloggning', // noun
-
+	'add_user' => 'L&auml;gg till en ny anv&auml;ndare',
+	'editing_user' => '&auml;ndrar %s&rsquo;s profil',
+	'remembered' => 'Jag kan mitt l&ouml;senord',
 	'forgotten_password' => 'Gl&ouml;mt l&ouml;senordet?',
 
-	'recover' => '&Aring;terh&auml;mta',
-	'editing' => 'Redigerar',
-	'profile' => 'profil',
+	// roles
+	'administrator' => 'Administrat&ouml;r',
+	'administrator_explain' => '',
 
+	'editor' => 'F&ouml;rfattare',
+	'editor_explain' => '',
+
+	'user' => 'Anv&auml;ndare',
+	'user_explain' => '',
+
+	// form fields
 	'real_name' => 'Namn',
-	'real_name_explain' => 'Anv&auml;ndarens riktiga namn. Anv&auml;nds i f&ouml;rfattarsignaturen (publikt synlig).',
+	'real_name_explain' => 'Anv&auml;ndarens riktiga namn. Anv&auml;nds i f&ouml;rfattarsignaturen (publikt synlig).',	
+	
 	'bio' => 'Biografi',
 	'bio_explain' => 'En kort anv&auml;ndarbiografi. Du kan anv&auml;nda giltig HTML.',
+
 	'status' => 'Status',
-	'inactive' => 'Inaktiv',
-	'active' => 'Aktiv',
-	'status_explain' => 'Om anv&auml;ndaren &auml;r inaktiv kan denne inte logga in.',
+	'status_explain' => '',
+
 	'role' => 'Roll',
-	'administrator' => 'Administrat&ouml;r',
-	'editor' => 'Redakt&ouml;r',
-	'user' => 'Anv&auml;ndare',
 	'role_explain' => 'Anv&auml;ndarens roll. Se <a href="//anchorcms.com/docs/roles">dokumentationen</a> f&ouml;r mer information.',
-	'user_details' => 'Anv&auml;ndarinformation',
-	'user_details_explain' => 'Skapa anv&auml;ndarinformationen.',
+
 	'username' => 'Anv&auml;ndarnamn',
 	'username_explain' => '&Ouml;nskat anv&auml;ndarnamn. Kan &auml;ndras vid ett senare tillf&auml;lle.',
+	'username_missing' => 'V&auml;nligen ange ett anv&auml;ndarnamn, m&aring;ste inneh&aring;lla minst %s tecken.',
+	'username_exists' => 'Anv&auml;ndarnamnet &auml;r upptaget.',
+
 	'password' => 'L&ouml;senord',
 	'password_explain' => '&Ouml;nskat l&ouml;senord. &Auml;ven detta kan &auml;ndras vid ett senare tillf&auml;lle.',
-	'password_blank' => 'L&auml;mna tomt om du inte vill &auml;ndra l&ouml;senordet.',
+	'password_too_short' => 'L&ouml;senord m&aring;ste inneh&aring;lla minst %s tecken',
+
+	'new_password' => 'Nytt l&ouml;senord',
+
 	'email' => 'Email',
 	'email_explain' => 'Anv&auml;ndarens email-adress. M&aring;ste anges ifall anv&auml;ndaren skulle gl&ouml;mma sitt l&ouml;senord.',
+	'email_missing' => 'Var god ange en giltig email-adress',
+	'email_not_found' => 'Profilen finns inte.',
 
-	'create' => 'Skapa',
-	'update' => 'Uppdatera',
-	'delete' => 'Ta bort',
-	'submit' => 'Skicka',
-	'return_users' => 'Tillbaka till anv&auml;ndare',
-
-	'username_exists' => 'Anv&auml;ndarnamnet &auml;r upptaget',
-	'missing_password' => 'Var god ange ett l&ouml;senord',
-	'invalid_email' => 'Var god ange en giltig email-adress',
-	'missing_name' => 'Var god ange ett riktigt namn',
-	'missing_username' => 'Var god ange ett anv&auml;ndarnamn',
-	'user_success_created' => 'En ny anv&auml;ndare har lagts till',
-	'user_success_updated' => 'Anv&auml;ndaren har uppdaterats',
-	'user_success_deleted' => 'Anv&auml;ndaren har tagits bort',
-	'user_success_password' => 'Ditt nya l&ouml;senord har skickats',
-	'user_notice_recover' => 'Vi har skickat ett email f&ouml;r att bekr&auml;fta ditt l&ouml;senordsbyte.',
-	'user_email_recover' => 'Du har beg&auml;rt att ditt l&ouml;senord ska &aring;terst&auml;llas. F&ouml;r att forts&auml;tta, var god f&ouml;lj l&auml;nken nedan.',
-	'user_subject_recover' => 'L&ouml;senords&aring;terst&auml;llning',
-	'invalid_account' => 'Konto ej hittat',
-	'missing_login_username' => 'Var god ange ditt anv&auml;ndarnamn',
-	'missing_login_password' => 'Var god ange ditt l&ouml;senord'
+	// messages
+	'updated' => 'Anv&auml;ndaren har uppdaterats.',
+	'created' => 'En ny anv&auml;ndare har lagts till.',
+	'deleted' => 'Anv&auml;ndaren har tagits bort.',
+	'delete_error' => 'Du kan inte radera din egen profil',
+	'login_error' => 'Anv&auml;ndaren eller l&ouml;senordet &auml;r felaktigt.',
+	'logout_notice' => 'Du &auml;r nu utloggad.',
+	'recovery_sent' => 'Vi har skickat ett email f&ouml;r att bekr&auml;fta ditt l&ouml;senordsbyte.',
+	'recovery_expired' => 'L&ouml;senords&aring;terst&auml;llningen har g&aring;tt ut, v&auml;nligen f&ouml;rs&ouml;k igen.',
+	'password_reset' => 'Ditt l&ouml;senord &auml;r nu &auml;ndrat. Nu kan du logga in!',	
+	
+	// password recovery email
+	'recovery_subject' => 'L&ouml;senords&aring;terst&auml;llning',
+	'recovery_message' => 'Du har beg&auml;rt att ditt l&ouml;senord ska &aring;terst&auml;llas.' .
+		'F&ouml;r att forts&auml;tta, var god f&ouml;lj l&auml;nken nedan.' . PHP_EOL . '%s',
 
 );
